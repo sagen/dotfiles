626d591431fd6b74da12065ffefe57e798fe7cac
Alexander Sagen <asagen@gmail.com>
2012-05-01 15:08:16 +0200

    dotfiles starts with .

---
+++ symlink.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import re
+from shutil import rmtree
+
+THIS_FILE = os.path.abspath(__file__)
+BASE = os.path.dirname(os.path.abspath(__file__)) + os.sep
+HOME = os.environ['HOME'] + os.sep
+esc_base = re.escape(BASE)
+
+IGNORE = re.compile(r'|'.join([re.escape(THIS_FILE),
+                               esc_base + r'\.git$',
+                               esc_base + r'\LICENSE$',
+                               esc_base + r'README.rdoc',
+                               esc_base + r'.*\.swp']))
+
+def force_link(source, target):
+    if os.path.islink(target):
+        os.remove(target)
+    else:
+        rmtree(target, True)
+
+    try:
+        os.symlink(source, target)
+    except OSError:
+        print "Could not symlink %s" % target
+
+def linkify(subpath):
+    based = BASE + subpath
+    homed = HOME + subpath
+
+    if IGNORE.match(based) is not None:
+        return
+
+    print 'OPERATING ON:', subpath
+
+    if not os.path.isdir(based):
+        force_link(based, homed)
+        return
+
+    if not os.path.isdir(homed) or os.path.islink(homed):
+        force_link(based, homed)
+        return
+
+    for item in os.listdir(based):
+        # recurse
+        linkify(subpath + os.sep + item)
+
+if __name__ == '__main__':
+    for item in os.listdir(BASE):
+        linkify(item)
